0bdc26156ceda5bade600a22ccedba2e8409c36e
moomyung1013 <moomyung1013@gmail.com>
2021-10-04 23:31:22 +0900

    Terrian 수정, 컴퓨터 스크립트 작성

---
+++ Hide-And-Seek/Assets/ComputerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerScript : MonoBehaviour
+{
+    Vector3 direction, default_direction;
+    float velocity, default_velocity, accelaration;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
